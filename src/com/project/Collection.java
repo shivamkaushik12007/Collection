3800dade39a820ab6ae1e297a896ab8fac2ecc72
kaushik <shivamkaushik12007@gmail.com>
2020-02-26 21:12:17 +0530

    the collection interface updated

--- src/com/project/Collection.java
+++ src/com/project/Collection.java
@@ -1,6 +1,12 @@
 package com.project;
 
 public interface Collection<T> {
-
+    public void add(T val);
+    public void add(int index,T val);
+    public void remove();
+    public void remove(int index);
+    public T get(int index);
+    public int size();
+    public boolean isEmpty();
 
 }
