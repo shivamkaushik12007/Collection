44d9dea61a29b562e17e346f52f45c8b62a2ce82
kaushik <shivamkaushik12007@gmail.com>
2020-02-29 22:01:00 +0530

    ...

--- src/com/project/TreeMap.java
+++ src/com/project/TreeMap.java
@@ -10,11 +10,11 @@ public class TreeMap<K,V> implements Map<K,V> {
     private Node<K,V> put(Node<K,V> root, K key, V val){
         if(root==null)
             return new Node(key,val);
-        if(root.key<key)
+        if(root.key.compareTo(key)<0)
             root.right=put(root.right,key,val);
-        if(root.key>key)
+        if(root.key.compareTo(key)>0)
             root.left=put(root.left,key,val);
-        if(root.key==key)
+        if(root.key.equals(key))
             root.val=val;
         return root;
     }
@@ -26,17 +26,17 @@ public class TreeMap<K,V> implements Map<K,V> {
     private V get(K key, Node<K,V> root){
         if(root==null)
             return null;
-        if(root.key==key)
+        if(root.key.equals(key))
             return root.val;
-        if(root.key>key)
+        if(root.key.compareTo(key)>0)
             return get(key, root.left);
-        if(root.key<key)
+        if(root.key.compareTo(key)<0)
             return get(key, root.right);
     }
 
     private class Node<K,V>{
         K key;
-        V value;
+        V val;
         Node<K,V> left;
         Node<K,V> right;
         Node(K key,V val){
